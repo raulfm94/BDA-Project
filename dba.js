4eb5a95515cd70340af67fa698ddfc86da8704aa
Memogcia <memogcia94@gmail.com>
2017-04-22 22:33:35 -0500

    inserting data into mongodb, renamed models, added some filters before inserting data

--- dba.js
+++ dba.js
@@ -1,6 +1,6 @@
 const mongoose    = require('mongoose');
-const airport    = require('./models/airport.js');
-const route      = require('./models/route.js');
+const airport     = require('./models/airport.js');
+const route       = require('./models/route.js');
 const recreatedb  = false;
 
 mongoose.connect('mongodb://localhost/airport');
@@ -14,20 +14,20 @@ if(recreatedb){
     var dataarray = array[i].split(',');
 
     if(dataarray[0] != null && dataarray[0] != '' && dataarray[1] != '' && dataarray[2] != '' && dataarray[3] != '' && dataarray[4] != '' && dataarray[5] != ''){
-      var airport = new airport({
-        id          :dataarray[0],
-        airportName :dataarray[1],
-        city        :dataarray[2],
-        country     :dataarray[3],
-        lat         :dataarray[4],
-        lng         :dataarray[5]
+      var object = new airport({
+        id          : parseInt(dataarray[0]),
+        airportName : dataarray[1],
+        city        : dataarray[2],
+        country     : dataarray[3],
+        lat         : parseFloat(dataarray[4]),
+        lng         : parseFloat(dataarray[5])
       });
-      airport.save(function(err)){
+      object.save(function(err){
         if(err){
           console.log(err);
           return;
         }
-      }
+      });
     }
   }
   console.log("Airports saved!");
@@ -38,19 +38,19 @@ if(recreatedb){
   for(i in array) {
     var dataarray = array[i].split(',');
 
-    if(dataarray[0] != null && dataarray[0] != '' && dataarray[0] != '' && dataarray[1] != '' && dataarray[2] != '' && dataarray[3] != ''){
-      var route = new route({
-        airlineId             :dataarray[0],
-        sourceAirportId       :dataarray[1],
-        destinationAirportId  :dataarray[2],
-        stops                 :dataarray[3]
+    if(dataarray[0] != null && dataarray[0] != '\N' && dataarray[0] != '\N' && dataarray[1] != '\N' && dataarray[2] != '\N' && dataarray[3] != '\N' && dataarray[1] != '' && dataarray[2] != '' && dataarray[3] != ''){
+      var object = new route({
+        airlineId             : parseInt(dataarray[0]),
+        sourceAirportId       : parseInt(dataarray[1]),
+        destinationAirportId  : parseInt(dataarray[2]),
+        stops                 : parseInt(dataarray[3])
       });
-      route.save(function(err)){
+      object.save(function(err){
         if(err){
           console.log(err);
           return;
         }
-      }
+      });
     }
   }
   console.log("Routes saved!");
